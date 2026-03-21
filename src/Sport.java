0ae916bee71a048cc0cf846d79cc001b4bedb877
ardakarakas <140883986+arda190@users.noreply.github.com>
2026-03-22 00:55:59 +0300

    Sport interface was created.

---
+++ src/Sport.java
@@ -0,0 +1,25 @@
+public interface Sport {
+
+    int getPlayersPerTeam(); //starting team
+    int getPlayersTotal();   // starting+substitute
+    int teamWinPoint();
+    int teamDrawPoint();
+    int teamLosePoint();
+    int maxSubstitute();
+    void updatePoints(Team home, Team away, int homeScore, int awayScore);
+    int getMaxRandomScore();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
