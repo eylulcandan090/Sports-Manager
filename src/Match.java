190620563f3878481542ab3877feb4d764383633
ardakarakas <140883986+arda190@users.noreply.github.com>
2026-03-22 00:23:02 +0300

    Match class is added.
    I create match class to use in MatchEngine and other things.

---
+++ src/Match.java
@@ -0,0 +1,68 @@
+public class Match{
+    Team home;
+    Team away;
+    private int homeScore;
+    private int awayScore;
+    private boolean isPlayed;
+
+
+
+    public Match(Team home,Team away){
+        this.home=home;
+        this.away=away;
+    }
+
+
+    public void startMatch(){
+
+    }
+
+    public Team getHome() {
+        return home;
+    }
+
+    public void setHome(Team home) {
+        this.home = home;
+    }
+
+    public Team getAway() {
+        return away;
+    }
+
+    public void setAway(Team away) {
+        this.away = away;
+    }
+
+    public int getHomeScore() {
+        return homeScore;
+    }
+
+    public void setHomeScore(int homeScore) {
+        this.homeScore = homeScore;
+    }
+
+    public int getAwayScore() {
+        return awayScore;
+    }
+
+    public void setAwayScore(int awayScore) {
+        this.awayScore = awayScore;
+    }
+
+    public boolean isPlayed() {
+        return isPlayed;
+    }
+
+    public void setPlayed(boolean played) {
+        isPlayed = played;
+    }
+    public void setScore(int homeScore,int awayScore){
+        this.homeScore=homeScore;
+        this.awayScore=awayScore;
+    }
+
+    public String getScore(){
+        return home.getTeamName() + " " + homeScore + " - " + awayScore + " " + away.getTeamName();
+    }
+
+}
