4016fb89ee96fac274e46e5b1479004c8f459636
eceaksut <eceaksut27@gmail.com>
2026-03-22 16:37:50 +0300

    Training system updated with focus and team players

--- src/TrainingSystem.java
+++ src/TrainingSystem.java
@@ -4,19 +4,34 @@ import java.util.Random;
 public abstract class TrainingSystem {
     protected Sport sport;
     protected Random rand;
+    protected String focusAttribute;
+
     public TrainingSystem(Sport sport) {
         this.sport=sport;
         this.rand=new Random();
     }
-    public abstract void applyTrainingEffect(Player player);
+    public abstract void applyTrainingEffect(Player player,String attribute);
     public abstract List<String> getTrainableAttributes();
-    public void conductTraining() {
+    public void conductTraining(Team team) {
+        if(team==null){
+            return;
+        }
         List<String> attributes=getTrainableAttributes();
         if(attributes != null && attributes.size()>0) {
-            rand.nextInt(attributes.size());
+            focusAttribute=attributes.get(rand.nextInt(attributes.size()));
+        }
+        for (Player p:team.getPlayerList()){
+            applyTrainingEffect(p,focusAttribute);
         }
     }
-    public String getSessionSummary(){
-        return "Training session";
+    public String getSessionSummary() {
+        if (focusAttribute == null) {
+            return "Training session";
+        }
+        return "Focus on: " + focusAttribute;
     }
-}
+    public void setFocus(String focusAttribute) {
+        this.focusAttribute = focusAttribute;
+    }
+    }
+
