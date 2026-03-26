67463ff4ed003b36490d3d1991b8acde2087dfda
eylulcandan090 <eeylulcandan@gmail.com>
2026-03-26 10:59:01 +0300

    Move Player class into src

---
+++ src/Player.java
@@ -0,0 +1,47 @@
+abstract class Player {
+    private String name;
+    private String strength;
+    private int age;
+    private String Nation;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStrength() {
+        return strength;
+    }
+
+    public void setStrength(String strength) {
+        this.strength = strength;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getNation() {
+        return Nation;
+    }
+
+    public void setNation(String nation) {
+        Nation = nation;
+    }
+
+
+
+
+
+
+
+
+}
