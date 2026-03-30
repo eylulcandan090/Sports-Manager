fc945f6ed9226475eb0ac757a9b8a14c9c0e79fa
eylulcandan090 <eeylulcandan@gmail.com>
2026-03-30 21:05:49 +0300

    add Substitution class with playerOut, playerIn and minute fields

--- src/Substitution.java
+++ src/Substitution.java
@@ -1,2 +1,28 @@
 public class Substitution {
+    private Player playerOut;
+    private Player playerIn;
+    private int minute;
+
+    public Substitution(Player playerOut, Player playerIn, int minute) {
+        this.playerOut = playerOut;
+        this.playerIn = playerIn;
+        this.minute = minute;
+    }
+
+    public Player getPlayerOut() {
+        return playerOut;
+    }
+
+    public Player getPlayerIn() {
+        return playerIn;
+    }
+
+    public int getMinute() {
+        return minute;
+    }
+
+    @Override
+    public String toString() {
+        return "Min " + minute + ": " + playerOut.getName() + " -> " + playerIn.getName();
+    }
 }
