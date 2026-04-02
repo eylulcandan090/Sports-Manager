f89adeda65d07ede2a531c9449e4e5e90a5a0de2
eylulcandan090 <eeylulcandan@gmail.com>
2026-04-02 09:01:22 +0300

    add and improve Coach class and update FootballTeam constructor to pass Football sport instance

--- src/main/java/Coach.java
+++ src/main/java/Coach.java
@@ -1,2 +1,38 @@
 public class Coach {
+    private String name;
+    private String specialization;
+    private int rating;
+
+    public Coach(String name, String specialization, int rating) {
+        this.name = name;
+        this.specialization = specialization;
+        this.rating = rating;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSpecialization() {
+        return specialization;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void trainPlayer(Player player) {
+        if (player == null) return;
+
+        int boost = rating / 20;
+        if (boost < 1) boost = 1;
+
+        int current = player.getAttribute(specialization);
+        player.setAttribute(specialization, current + boost);
+    }
+
+    @Override
+    public String toString() {
+        return name + " | " + specialization + " | Rating: " + rating;
+    }
 }
--- src/main/java/FootballTeam.java
+++ src/main/java/FootballTeam.java
@@ -1,47 +1,26 @@
-public class FootballTeam extends Team{
-    private static final int MAX_PLAYERS=11;
-    private static final  int MAX_SUBSTITUTES=12;
+public class FootballTeam extends Team {
+    private static final int MAX_PLAYERS = 11;
+    private static final int MAX_SUBSTITUTES = 12;
     private static final int MAX_SUBSTITUTIONS = 5;
-    private static final int MAX_TEAM_PLAYERS=23;
-
-
-
+    private static final int MAX_TEAM_PLAYERS = 23;
 
     public FootballTeam(String teamName, String coachName) {
-        super(teamName, coachName);
+        super(new Football(), teamName, coachName);
     }
 
-
-
-    public void addPlayerToTeam(Player player){
-        if(this.getTeamPlayerCount()<MAX_TEAM_PLAYERS){
-           this.addPlayer(player);
+    public void addPlayerToTeam(Player player) {
+        if (this.getTeamPlayerCount() < MAX_TEAM_PLAYERS) {
+            this.addPlayer(player);
         }
     }
 
-
-    public void addPlayerToSubstitute(Player player){
-        if(this.getTeamSubstituteCount()<MAX_SUBSTITUTES){
+    public void addPlayerToSubstitute(Player player) {
+        if (this.getTeamSubstituteCount() < MAX_SUBSTITUTES) {
             this.addSubstitute(player);
         }
     }
 
-
-
-
-    @Override
-    public int getPlayersOnField(){
-        return this.MAX_PLAYERS;
+    public int getPlayersOnField() {
+        return MAX_PLAYERS;
     }
-
-
-
-
-
-
-
-
-
-
-
 }
