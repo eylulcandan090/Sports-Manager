60c9859f8c5849ff5ae809d4d7b6805a165e317d
ardakarakas <140883986+arda190@users.noreply.github.com>
2026-04-03 12:37:11 +0300

    MatchResultTest was created

---
+++ src/test/java/MatchResultTest.java
@@ -0,0 +1,31 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MatchResultTest {
+
+    @Test
+    void testEnumValues() {
+        MatchResult[] results=MatchResult.values();
+        assertEquals(3,results.length,"MatchResult should have exactly 3 values");
+        assertEquals(MatchResult.HOME_WIN,MatchResult.valueOf("HOME_WIN"));
+        assertEquals(MatchResult.AWAY_WIN,MatchResult.valueOf("AWAY_WIN"));
+        assertEquals(MatchResult.DRAW,MatchResult.valueOf("DRAW"));
+    }
+
+    @Test
+    void testMatchResultIntegrationWithMatch() {
+        Sport football=new Football();
+        Team home=new FootballTeam(football,"Home","Coach1","Stadium1");
+        Team away=new FootballTeam(football,"Away","Coach2","Stadium2");
+
+        Match match=new Match(home, away);
+        match.setResult(MatchResult.HOME_WIN);
+        assertEquals(MatchResult.HOME_WIN, match.getResult());
+
+        match.setResult(MatchResult.AWAY_WIN);
+        assertEquals(MatchResult.AWAY_WIN, match.getResult());
+
+        match.setResult(MatchResult.DRAW);
+        assertEquals(MatchResult.DRAW, match.getResult());
+    }
+}
