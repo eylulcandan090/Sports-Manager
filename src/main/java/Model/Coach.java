8068642ec544c1a9b90719c4d01888a075989620
ardakarakas <140883986+arda190@users.noreply.github.com>
2026-04-22 12:18:44 +0300

    Refactor Coach class to update attributes and methods
    Refactored Coach class to include trainingType and skillLevel. Removed specialization and rating attributes along with related methods.

--- src/main/java/Model/Coach.java
+++ src/main/java/Model/Coach.java
@@ -1,40 +1,39 @@
 package Model;
 
 public class Coach {
+    private int id;
     private String name;
-    private String specialization;
-    private int rating;
+    private TrainingType trainingType;
+    private int skillLevel;
+    private int teamId;
 
-    public Coach(String name, String specialization, int rating) {
+
+    public Coach(String name, TrainingType trainingType, int skillLevel, int teamId) {
         this.name = name;
-        this.specialization = specialization;
-        this.rating = rating;
+        this.trainingType = trainingType;
+        this.skillLevel = skillLevel;
+        this.teamId = teamId;
     }
 
-    public String getName() {
-        return name;
+    public void train(FootballPlayer player){
+        switch (trainingType){
+            case SHOOT:
+                player.setShooting(player.getShooting()+skillLevel);
+                break;
+            case PASS:
+                player.setPassing(player.getPassing()+skillLevel);
+                break;
+            case GOALKEEPER:
+                player.setGoalkeeping(player.getGoalkeeping()+skillLevel);
+                break;
+            case DEFANCE:
+                player.setDefance(player.getDefance()+skillLevel);
+                break;
+        }
     }
 
-    public String getSpecialization() {
-        return specialization;
-    }
 
-    public int getRating() {
-        return rating;
-    }
 
-    public void trainPlayer(Player player) {
-        if (player == null) return;
 
-        int boost = rating / 20;
-        if (boost < 1) boost = 1;
 
-        int current = player.getAttribute(specialization);
-        player.setAttribute(specialization, current + boost);
-    }
-
-    @Override
-    public String toString() {
-        return name + " | " + specialization + " | Rating: " + rating;
-    }
 }
