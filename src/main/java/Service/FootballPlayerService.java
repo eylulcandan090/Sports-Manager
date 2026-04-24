2155288052b9973c47f41f72cb0803bbb930827f
arda190 <aardaa_karakas@outlook.com>
2026-04-24 18:56:41 +0300

    Service classes are created.

---
+++ src/main/java/Service/FootballPlayerService.java
@@ -0,0 +1,40 @@
+package Service;
+
+import Model.Coach;
+import Model.Football.FootballPlayer;
+import Repository.CoachRepo;
+import Repository.Football.FootballPlayerRepo;
+
+public class FootballPlayerService {
+    private FootballPlayerRepo playerRepo;
+    private CoachRepo coachRepo;
+
+    public FootballPlayerService(FootballPlayerRepo footballPlayerRepo,CoachRepo coachRepo){
+        this.playerRepo=footballPlayerRepo;
+        this.coachRepo=coachRepo;
+    }
+
+    public void applyTraining(int player_id,int coachId){
+        FootballPlayer player=playerRepo.getFootballPlayerById(player_id);
+        Coach coach=coachRepo.getCoachById(coachId);
+
+        if(player!=null && coach!=null){
+            coach.train(player);
+            playerRepo.updatePlayer(player);
+        }
+    }
+
+
+    public void save(String name, int age, int injuryStatus, int team_id, int shooting, int passing, int goalkeeping,String position){
+        playerRepo.addFootballPlayer(name,age,injuryStatus,team_id,shooting,passing,goalkeeping,position);
+    }
+
+
+
+
+
+
+
+
+
+}
---
+++ src/main/java/Service/SportService.java
@@ -0,0 +1,36 @@
+package Service;
+
+import Model.SportEntity;
+import Repository.SportRepo;
+
+import java.util.ArrayList;
+
+public class SportService {
+    private SportRepo repo;
+
+    public SportService(SportRepo sportRepo){
+        this.repo=sportRepo;
+    }
+
+    public void saveSport(String name){
+        repo.addSport(name);
+    }
+
+    public ArrayList<String> getSports(){
+        return repo.getAllSports();
+    }
+
+
+    public int getOrSave(String sport){
+        int id=repo.findByName(sport);
+
+        if(id==-1){
+            saveSport(sport);
+            return repo.findByName(sport);
+        }
+        else{
+            return id;
+        }
+    }
+
+}
