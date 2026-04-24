9bc8423d6774e434e7147c9ef2ddfcbebb83568c
arda190 <aardaa_karakas@outlook.com>
2026-04-24 18:36:47 +0300

    Some Database tables are updated.

--- src/main/java/Database/Database.java
+++ src/main/java/Database/Database.java
@@ -1,9 +1,6 @@
 package Database;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class Database {
     private static Database database;
@@ -12,11 +9,13 @@ public class Database {
     private Database(){
         connect();
         enableForeignKeys();
-        createFootballLeague();
+        createLeague();
         createFootballPlayerTable();
-        createFootballTeamsTable();
+        createTeamsTable();
         createMatchTable();
         createCoachTables();
+        createGameTable();
+        createSportTable();
     }
 
     public static Database getInstance(){
@@ -45,10 +44,12 @@ public class Database {
     }
 
 
-    private void createFootballLeague(){
-        String query="CREATE TABLE IF NOT EXISTS football_leagues ( "+
+    private void createLeague(){
+        String query="CREATE TABLE IF NOT EXISTS leagues ( "+
                 "id INTEGER PRIMARY KEY AUTOINCREMENT,"+
-                "name TEXT NOT NULL);";
+                "name TEXT NOT NULL,"+
+                "sport_id INTEGER,"+
+                "FOREIGN KEY(sport_id) REFERENCES sports(id) )";
         try (Statement stmt=connection.createStatement()){
             stmt.execute(query);
         }catch(SQLException s){
@@ -56,12 +57,15 @@ public class Database {
         }
     }
 
-    private void createFootballTeamsTable(){
-        String query="CREATE TABLE  IF NOT EXISTS football_teams("+
+    private void createTeamsTable(){
+        String query="CREATE TABLE  IF NOT EXISTS teams("+
                 "id INTEGER PRIMARY KEY AUTOINCREMENT,"+
                 "team_name TEXT NOT NULL,"+
                 "league_id INTEGER,"+
-                "FOREIGN KEY(league_id) REFERENCES football_leagues(id) )";
+                "points INTEGER DEFAULT 0,"+
+                "sport_id INTEGER,"+
+                "FOREIGN KEY(league_id) REFERENCES leagues(id),"+
+                "FOREIGN KEY(sport_id) REFERENCES sports(id) )";
 
         try(Statement stmt=connection.createStatement()) {
             stmt.execute(query);
@@ -76,11 +80,14 @@ public class Database {
         String query="CREATE TABLE IF NOT EXISTS football_players("+
                 "id INTEGER PRIMARY KEY AUTOINCREMENT,"+
                 "name TEXT NOT NULL,"+
-                "rating INTEGER NOT NULL,"+
+                "age INTEGER,"+
                 "position TEXT,"+
-                "injured BOOLEAN,"+
+                "injuryStatus INTEGER,"+
+                "shooting INTEGER,"+
+                "passing INTEGER,"+
+                "goalkeeping INTEGER,"+
                 "team_id INTEGER,"+
-                "FOREIGN KEY(team_id) REFERENCES football_teams(id) )";
+                "FOREIGN KEY(team_id) REFERENCES teams(id) )";
         try(Statement stmt=connection.createStatement()) {
             stmt.execute(query);
         }catch(SQLException s){
@@ -90,7 +97,6 @@ public class Database {
 
     private void enableForeignKeys() {
         try(Statement stmt=connection.createStatement()){
-
             stmt.execute("PRAGMA foreign_keys = ON");
         } catch (SQLException e) {
             e.printStackTrace();
@@ -106,8 +112,8 @@ public class Database {
                 " home_score INTEGER DEFAULT 0," +
                 " away_score INTEGER DEFAULT 0," +
                 " match_date TEXT," +
-                " FOREIGN KEY(home_team_id) REFERENCES football_teams(id)," +
-                " FOREIGN KEY(away_team_id) REFERENCES football_teams(id)" +
+                " FOREIGN KEY(home_team_id) REFERENCES teams(id)," +
+                " FOREIGN KEY(away_team_id) REFERENCES teams(id)" +
                 ");";
         try(Statement stmt=connection.createStatement()){
             stmt.execute(query);
@@ -122,20 +128,46 @@ public class Database {
                 "id INTEGER PRIMARY KEY AUTOINCREMENT,"+
                 "coach_name VARCHAR(50) NOT NULL,"+
                 "team_id INTEGER, "+
-                "FOREIGN KEY(team_id) REFERENCES football_teams(id) )";
+                "FOREIGN KEY(team_id) REFERENCES teams(id) )";
 
         try(Statement stmt=connection.createStatement()){
             stmt.execute(query);
         }catch(SQLException s){
             System.out.println(s.getMessage());
         }
-
     }
 
+    private void createGameTable(){
+        String query="CREATE TABLE IF NOT EXISTS game_status(" +
+                "id INTEGER PRIMARY KEY AUTOINCREMENT," +
+                "isStarted INTEGER," +
+                "selected_teamId INTEGER," +
+                "FOREIGN KEY(selected_teamId) REFERENCES teams(id) )";
+
+        String insert= "INSERT INTO game_status(id,isStarted) " +
+                "SELECT 1,0 WHERE NOT EXISTS (SELECT 1 FROM game_status)";
+
+        try (Statement statement=connection.createStatement()) {
+            statement.execute(query);
+            statement.executeUpdate(insert);
 
 
+        } catch (SQLException sqlException) {
+            System.out.println(sqlException.getMessage());
+        }
+    }
 
+    private void createSportTable(){
+        String query="CREATE TABLE IF NOT EXISTS sports("+
+                "id INTEGER PRIMARY KEY AUTOINCREMENT,"+
+                "sport_name VARCHAR(40) )";
 
+        try(Statement statement=connection.createStatement()){
+            statement.executeUpdate(query);
+        }catch(SQLException sqlException){
+            System.out.println(sqlException.getMessage());
+        }
+    }
 
 
 
