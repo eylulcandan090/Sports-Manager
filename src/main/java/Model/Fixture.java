277e50afe90aeee39fbf11a6720f6b7456cc2d48
ardakarakas <140883986+arda190@users.noreply.github.com>
2026-04-27 23:48:31 +0300

    Refactor Fixture class with new attributes and methods
    Refactored Fixture class to include homeId, awayId, week, and isPlayed attributes with corresponding getters and setters.

--- src/main/java/Model/Fixture.java
+++ src/main/java/Model/Fixture.java
@@ -1,34 +1,48 @@
 package Model;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 public class Fixture {
+    private int week;
+    private int homeId;
+    private int awayId;
+    private boolean isPlayed;
 
-    private final Map<Integer, List<Match>> weeklyMatches;
-    private final int totalWeeks;
 
-    public Fixture(int totalWeeks) { //constructor
-        this.totalWeeks = totalWeeks;
-        this.weeklyMatches = new HashMap<>();
+    public Fixture(int homeId, int awayId,int week, boolean isPlayed) {
+        this.homeId = homeId;
+        this.awayId = awayId;
+        this.week=week;
+        this.isPlayed = isPlayed;
     }
 
-    //methods
-    public void addMatch(int week, Match match) {
-        weeklyMatches.computeIfAbsent(week, k -> new ArrayList<>()).add(match);
+    public int getWeek() {
+        return week;
     }
 
-    public List<Match> getWeek(int week) {
-        return weeklyMatches.getOrDefault(week, new ArrayList<>());
+    public void setWeek(int week) {
+        this.week = week;
     }
 
-    public int getTotalWeeks() {
-        return totalWeeks;
+    public int getHomeId() {
+        return homeId;
     }
 
-    public Map<Integer, List<Match>> getWeeklyMatches(){
-        return weeklyMatches;
+    public void setHomeId(int homeId) {
+        this.homeId = homeId;
+    }
+
+    public int getAwayId() {
+        return awayId;
+    }
+
+    public void setAwayId(int awayId) {
+        this.awayId = awayId;
+    }
+
+    public boolean getIsPlayed() {
+        return isPlayed;
+    }
+
+    public void setIsPlayed(boolean isPlayed) {
+        this.isPlayed = isPlayed;
     }
 }
