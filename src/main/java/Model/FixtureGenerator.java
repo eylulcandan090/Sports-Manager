10f391ac7b043df55732324f77f00d04e4823e2c
ardakarakas <140883986+arda190@users.noreply.github.com>
2026-04-29 00:13:45 +0300

    Add import for Statement in FixtureGenerator.java

--- src/main/java/Model/FixtureGenerator.java
+++ src/main/java/Model/FixtureGenerator.java
@@ -2,6 +2,7 @@ package Model;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.Statement;
 import java.sql.SQLException;
 import java.util.Collections;
 import java.util.List;
