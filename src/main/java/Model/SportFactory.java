ee06c43507308e7b4b8725a12b0c4f6f94f33a8e
ardakarakas <140883986+arda190@users.noreply.github.com>
2026-04-29 19:53:44 +0300

    Refactor SportFactory to create sports dynamically

--- src/main/java/Model/SportFactory.java
+++ src/main/java/Model/SportFactory.java
@@ -1,7 +1,16 @@
 package Model;
 
-public interface SportFactory {
-    public Player createPlayer();
-    public TrainingStrategy createTraining(TrainingType type);
-    public Coach createCoach();
+import Model.Basketball.Basketball;
+import Model.Football.Football;
+
+public class SportFactory {
+   public Sport createSport(String type){
+       switch (type.toLowerCase()){
+           case "football":
+               return new Football();
+           case "basketball":
+               return new Basketball();
+       }
+       throw new IllegalArgumentException();
+   }
 }
