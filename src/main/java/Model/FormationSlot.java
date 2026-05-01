c843194ccf5c74b524f92fd48e4f8d4bf5e0cb89
arda190 <aardaa_karakas@outlook.com>
2026-05-01 21:09:34 +0300

    To store match squad FormationSlot is created.

---
+++ src/main/java/Model/FormationSlot.java
@@ -0,0 +1,22 @@
+package Model;
+
+public class FormationSlot {
+    private String position;
+    private Player player;
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public void setPlayer(Player player) {
+        this.player = player;
+    }
+}
